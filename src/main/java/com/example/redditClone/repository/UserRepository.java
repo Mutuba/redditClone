e533692121343d9b6e8492a384109e906115481f
Daniel Mutuba <danielmutubait@gmail.com>
2020-08-28 17:33:14 +0300

    Add login and retur auth token

--- src/main/java/com/example/redditClone/repository/UserRepository.java
+++ src/main/java/com/example/redditClone/repository/UserRepository.java
@@ -8,3 +8,30 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
+
+//interface TodoRepository extends Repository<Todo, Long> {
+//
+//    List<Todo> findByDescriptionContainsOrTitleContainsAllIgnoreCase(String descriptionPart,
+//                                                                     String titlePart);
+//}
+//Spring Data JPA uses the descriptionPart method parameter when it ensures
+//        that the description of the returned todo entry contains the given search term.
+//        Spring Data JPA uses the titlePart method parameter when it ensures
+//        that the title of the returned todo entry contains the given search term.
+
+//JPQL
+//
+//interface TodoRepository extends Repository<Todo, Long> {
+//
+//    @Query("SELECT t FROM Todo t WHERE t.title = 'title'")
+//    public List<Todo> findByTitle();
+//}
+//
+//SQL
+//interface TodoRepository extends Repository<Todo, Long> {
+//
+//    @Query(value = "SELECT * FROM todos t WHERE t.title = 'title'",
+//            nativeQuery=true
+//    )
+//    public List<Todo> findByTitle();
+//}
--- src/main/java/com/example/redditClone/security/JwtTokenProvider.java
+++ src/main/java/com/example/redditClone/security/JwtTokenProvider.java
@@ -4,11 +4,10 @@ import com.example.redditClone.service.UserPrincipal;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
 // provides a user token based on a user who is trying to login
