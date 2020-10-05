b0367cd9ec10571179f72fd1760b3a53cb03a1d6
Mutuba <danielmutubait@gmail.com>
2020-10-05 14:57:25 +0300

    Fix webconfig for swagger docs

--- src/main/java/com/example/redditClone/config/Constants.java
+++ src/main/java/com/example/redditClone/config/Constants.java
@@ -2,6 +2,7 @@ package com.example.redditClone.config;
 
 import lombok.experimental.UtilityClass;
 
+
 @UtilityClass
 public class Constants {
     public static final String EMAIL_ACTIVATION = "http://localhost:8080/api/auth/verify";
