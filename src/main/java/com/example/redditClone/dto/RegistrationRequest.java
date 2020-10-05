05b1466d04a8a6dadd4288c2ec8f987f6a5aad68
Mutuba <danielmutubait@gmail.com>
2020-10-05 13:25:06 +0300

    Refactor and remove duplicates from pom.xml

--- src/main/java/com/example/redditClone/dto/RegistrationRequest.java
+++ src/main/java/com/example/redditClone/dto/RegistrationRequest.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegistrationRequest {
-
     @NotBlank
     private String username;
 
--- src/test/java/com/example/redditClone/security/JwtTokenProviderTest.java
+++ src/test/java/com/example/redditClone/security/JwtTokenProviderTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.Date;
 
